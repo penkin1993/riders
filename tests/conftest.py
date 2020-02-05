0a3a99f7e77c7d8da0e4d471be31fc2e7dc967a1
Григорий Пенкин <penkin.g@yandex.ru>
2020-02-06 01:40:55 +0300

    desc

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,22 +6,24 @@ import numpy as np
 sys.path.append("..")
 
 from riders_combination_storage import RidersCombinationsStorage
-from riders_iterator import CombinationsChecker
+from riders_iterator import RidersCombinationsChecker
 
 
 @pytest.fixture(scope="module")
 def rcs():
     rcs_object = RidersCombinationsStorage({"a": (2, 3), "b": (2, 15), "c": (0, 7), "d": (13, 16), "e": (14, 16)})
 
-    array = rcs_object.get_combinations(("d",), ("c",))
-    ind = [1, 3, 45, 5]
-    new_array = array[ind, :]
-    rcs_object.set_combinations(("d",), ("c",), ind, new_array)
+    array, not_duplicate_index = rcs_object.get_combinations(("d",), ("c",))
+    ind = np.array([1, 3, 45, 5])
 
-    array_ = rcs_object.get_combinations(("d", "c"), ("c",))
-    ind_ = [1, 45]
+    new_array = array[ind, :]
+    rcs_object.set_combinations(("d", "c"), not_duplicate_index, ind, new_array)
+
+    array_, not_duplicate_index_ = rcs_object.get_combinations(("d", "c"), ("c",))
+    ind_ = np.array([1, 45])
+
     new_array_ = array_[ind_, :]
-    rcs_object.set_combinations(("d", "c"), ("c",), ind_, new_array_)
+    rcs_object.set_combinations(("d", "c", "c",), not_duplicate_index_, ind_, new_array_)
 
     yield rcs_object
 
@@ -31,7 +33,7 @@ def rcs():
 @pytest.fixture(scope="module")
 def rc():
     rider_requirements = np.array([2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0])
-    comb_check = CombinationsChecker(rider_requirements)
+    comb_check = RidersCombinationsChecker(rider_requirements)
 
     yield comb_check
 
--- tests/test_riders_combination_storage.py
+++ tests/test_riders_combination_storage.py
@@ -3,23 +3,5 @@ import numpy as np
 
 def test_rcs_get_combinations_and_set_combinations_simple(rcs):
 
-    assert rcs.get_combinations(("d",), ("c",)).shape == (63, 17)
-    assert rcs.get_combinations(("d", "c"), ("c",)).shape == (84, 17)
-
-
-def test_rcs_get_best_combination_and_set_best_combination_simple(rcs):  # TODO: Добавить фикстуру
-
-    rcs.best_combination = ("c",), np.array([0, 1, 4]), np.array([100, 10, 20])
-    assert rcs.best_combination == (('c',), ((0, 3),), 1)
-
-    rcs.best_combination = ("d", "c",), np.array([12, 3, 4]), np.array([10, 10, 10])
-    assert rcs.best_combination == (('c',), ((0, 3),), 1)
-
-    rcs.best_combination = ("c", "q",), np.array([31, 0, 12]), np.array([20, 20, 20])
-    assert rcs.best_combination == (('c',), ((0, 3),), 1)
-
-    rcs.best_combination = ("c",), np.array([0, 1, 4]), np.array([100, 10, 20])
-    assert rcs.best_combination == (('c',), ((0, 3),), 1)
-
-    rcs.best_combination = ("d", "c", "c"), np.array([1, 2]), np.array([1, 5])
-    assert rcs.best_combination == (('d', 'c', 'c'), ((14, 16), (0, 5), (0, 5)), 1)
+    assert rcs.get_combinations(("d",), ("c",))[0].shape == (63, 17)
+    assert rcs.get_combinations(("d", "c"), ("c",))[0].shape == (82, 17)
